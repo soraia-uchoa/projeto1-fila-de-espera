8813bb4f654acaf63cc38908db5305bd4a7fa6d0
soraia-uchoa <soraiavercosa3@icloud.com>
2024-10-24 09:39:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-lert waitingLine = ('fila de espera')
+alert waitingLine = ('fila de espera')
 let queue = ('paciente 1','paciente2','paciente3')
+var waitingLine = ["1 - nome","2 - consulta","3 - sair"];
+   
+var waitingLinePop = waitingLine.pop();
+const.patient = ['paciente 1','paciente 2','paciente 2']
 
